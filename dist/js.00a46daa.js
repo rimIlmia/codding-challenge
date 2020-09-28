0bfb07ddfd3a9e14ec738d7412df157121c3d6d0
rimilmia <rim.chakroun.pro@gmail.com>
2020-09-28 16:30:39 +0200

    burger-menu

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -118,7 +118,19 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/index.js":[function(require,module,exports) {
+function clicked() {
+  var x = document.getElementById("nav");
 
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  console.log("bonjour");
+}
+
+document.getElementById("btn").addEventListener("click", clicked);
 },{}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +159,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53686" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55668" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
